dbbf8d9178a192e37c5c149350fb65231f04fe42
Muluken Mekuriya <85693465+MulukenMekuriya@users.noreply.github.com>
2023-02-09 11:25:50 -0500

    fetch tours

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours }) => {
+	return (
+		<main>
+			{/* {tours.map((tour) => {
+				return <Tour />;
+			})} */}
+			<Tour />
+		</main>
+	);
 };
 
 export default Tours;
