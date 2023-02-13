ed59da52a5b4f5ad46d7351b4fe98aa3caa66070
Muluken Mekuriya <85693465+MulukenMekuriya@users.noreply.github.com>
2023-02-13 11:25:22 -0500

    add todo is completed

--- 24-TODO/todo/src/App.js
+++ 24-TODO/todo/src/App.js
@@ -2,10 +2,31 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [todos, setTodos] = useState([]);
+	const [inputeTodos, setInputeTodos] = useState("");
+
+	const handleAddTodo = () => {
+		setTodos([...todos, inputeTodos]);
+		setInputeTodos("");
+	};
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1 className="text-center mb-4">Todo List</h1>
+				<div>
+					<input
+						type="text"
+						value={inputeTodos}
+						onChange={(e) => setInputeTodos(e.target.value)}
+					/>
+					<button onClick={handleAddTodo}>Add Todo</button>
+				</div>
+				<ul>
+					{todos.map((todo, index) => (
+						<li key={index}>{todo}</li>
+					))}
+				</ul>
 			</header>
 		</div>
 	);
