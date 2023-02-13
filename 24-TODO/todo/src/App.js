ac91eec8b37ae86abfd0e843cf39aec1cf596481
Muluken Mekuriya <85693465+MulukenMekuriya@users.noreply.github.com>
2023-02-13 10:33:58 -0500

    form created for the inpute files

--- 24-TODO/todo/src/App.js
+++ 24-TODO/todo/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [Todo, setTodo] = useState([]);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<form action="">
+					<div>
+						<input type="text" /> <button>Add ToDo</button>
+					</div>
+					{/* This would be list of todos added from the state change */}
+					<div></div>
+				</form>
+			</header>
+		</div>
+	);
 }
 
 export default App;
